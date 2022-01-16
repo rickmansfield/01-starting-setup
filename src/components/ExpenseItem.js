2dfad31e10c325894776880605158494c52c3abc
Rick <ricksmycodeguy@gmail.com>
2022-01-16 17:33:46 -0600

    separates concerns creates ExpenseDate.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+
 function ExpenseItem(props) {
-    console.log('props', props);
-    const month = props.date.toLocaleString('en-us', { month: 'short' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = props.date.getFullYear();
     return (
 
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
-            <div>{props.date.toISOString()}</div>
+        <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
