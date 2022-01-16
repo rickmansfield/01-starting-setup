634d76b10a84674f4aa559b09b2674f2998e831d
Rick <ricksmycodeguy@gmail.com>
2022-01-16 11:29:09 -0600

    adds dynamic date to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
     console.log('props', props);
+    const month = props.date.toLocaleString('en-us', { month: 'short' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
     return (
+
         <div className='expense-item'>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
