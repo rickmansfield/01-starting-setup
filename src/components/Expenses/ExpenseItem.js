57dedb77511cc67c2d7f81e10cd3642dc6aa2ec5
Rick <ricksmycodeguy@gmail.com>
2022-01-17 14:21:44 -0600

    Adds state to title in ExpenseItems.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) =>  {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHander = () => {
+    setTitle('Updated!');
+    console.log('title', title);
+  }
     return (
 
         <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
-            </div>
+        </div>
+        <button onClick={clickHander}>Change Title</button>
         </Card>
     )
 }
 
 export default ExpenseItem
+
+
