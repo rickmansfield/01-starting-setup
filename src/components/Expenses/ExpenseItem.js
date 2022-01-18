8cdab23d4462dad374995bfd4ac420810064349d
Rick <ricksmycodeguy@gmail.com>
2022-01-18 16:56:46 -0600

    Converts hard code to dynamic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) =>  {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHander = () => {
-    setTitle('Updated!');
-    console.log('title', title);
-  }
+  // const clickHander = () => {
+  //   setTitle('Updated!');
+  //   console.log('title', title);
+  // }
     return (
 
         <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={clickHander}>Change Title</button>
+        {/* <button onClick={clickHander}>Change Title</button> */}
         </Card>
     )
 }
