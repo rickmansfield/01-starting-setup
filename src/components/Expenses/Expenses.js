3f0b631f2e43aedef496c6b1152874e9d6ceca4e
Rick <ricksmycodeguy@gmail.com>
2022-01-20 11:32:06 -0600

    adds fill to chart bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -36,6 +37,7 @@ const Expenses = (props) => {
           filteredYear={filteredYear}
           filterChangeHandler={filterChangeHandler}
         />
+        <ExpensesChart expenses={ filteredExpenses }/>
         <ExpensesList items={ filteredExpenses }/>
         {/* {filteredExpenses.length === 0 ? <p>No Expenses Found!</p> : filteredExpenses.map((expense) => (
           <ExpenseItem
