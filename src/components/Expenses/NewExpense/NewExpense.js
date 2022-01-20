e33d591c4e3f23edd366160dbe9f2aca8761b92e
Rick <ricksmycodeguy@gmail.com>
2022-01-20 09:35:15 -0600

    Completes cancel button feature

--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -17,14 +17,17 @@ const NewExpense = (props) => {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    console.log('expenseData', expenseData);
     props.onAddExpense(expenseData)
+    setIsEditing(false);
   };
   return (
     <div className='new-expense'>
       {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
-      {isEditing && <ExpenseForm onSaveExpenseData={saveExpeneseDataHandler} stopEditingHandler={stopEditingHandler}/>}
-      
+      {isEditing &&
+        <ExpenseForm
+          onSaveExpenseData={saveExpeneseDataHandler}
+          stopEditingHandler={stopEditingHandler}
+        />}
     </div>
   )
 }
